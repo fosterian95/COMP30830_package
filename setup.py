7836d6c5c480458a75a5978b52870acc87aafa27
Ian Foster <fosterian95@gmail.com>
2022-01-25 10:34:52 +0000

    First Commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='practical2_package',
+      version='0.1',
+      description='Package created for COMP30830 practical 2',
+      url='http://github.com/storborg/funniest',
+      author='fosterian95',
+      author_email='fosterian95@gmail.com',
+      license='MIT',
+      packages=['practical2_package'],
+      zip_safe=False)
